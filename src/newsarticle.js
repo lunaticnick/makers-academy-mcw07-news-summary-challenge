0e5bd6c6e18d0535366e27d60f58957293cdc4e3
Nikolaos <lunaticnick@hotmail.com>
2018-01-13 23:25:44 +0000

    Test for article instantiation - passed

--- src/newsarticle.js
+++ src/newsarticle.js
@@ -1,1 +1,14 @@
+(function(exports){
 
+  function Article (headline,url,body){
+    this.headline = headline;
+    this.url = url;
+    this.body = body;
+  };
+
+
+
+exports.Article = Article
+
+
+})(this);
