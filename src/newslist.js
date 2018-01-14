9a45f000332ce8d8fc36770d56c9df97f337e1bc
Nikolaos <lunaticnick@hotmail.com>
2018-01-14 19:13:56 +0000

    Test for article added to news list - passed

--- src/newslist.js
+++ src/newslist.js
@@ -3,6 +3,15 @@
     this.articleslist = [];
   }
 
+  NewsList.prototype.showAllArticles = function (article){
+    return this.articleslist;
+  };
+
+
+  NewsList.prototype.addArticle = function (article){
+    this.articleslist.push(article);
+  };
+
     exports.NewsList = NewsList;
 
 })(this);
